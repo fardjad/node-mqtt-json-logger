a49613dcc516c6da63229f1864b86de23702b261
Fardjad Davari <public@fardjad.com>
2020-03-10 05:17:38 +0330

    Extract MQTT broker logic into its own class

---
+++ src/broker/broker.ts
@@ -0,0 +1,22 @@
+import { EventEmitter } from "ee-ts";
+
+export interface BrokerEvents {
+  message(topic: string, payload: any): void;
+}
+
+export type TopicSubscription<TopicSubscriptionOptions> = {
+  topic: string;
+  options: TopicSubscriptionOptions;
+};
+
+export default interface Broker<
+  BrokerClient,
+  BrokerOptions,
+  TopicSubscriptionOptions
+> extends EventEmitter<BrokerEvents> {
+  connect(
+    options: BrokerOptions,
+    subscriptions: TopicSubscription<TopicSubscriptionOptions>[]
+  ): Promise<BrokerClient>;
+  disconnect(): Promise<void>;
+}
